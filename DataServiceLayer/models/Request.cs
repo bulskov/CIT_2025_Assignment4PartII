e80859959372b98824563fa50c7aa3a005158327
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-16 16:49:07 +0200

    Add Request and Response models to the DataServiceLayer

---
+++ DataServiceLayer/models/Request.cs
@@ -0,0 +1,10 @@
+namespace DataServiceLayer.Models
+{
+    public class Request
+    {
+        public string? Method { get; set; }  
+        public string? Path { get; set; }    
+        public string? Date { get; set; }   
+        public string? Body { get; set; }  
+    }
+}
