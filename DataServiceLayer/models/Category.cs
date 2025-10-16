84c29a48ba7fc87414a540dddc51ddb0052ed4ec
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-16 17:20:43 +0200

    Update Category model to allow nullable Name and Description properties

--- DataServiceLayer/models/Category.cs
+++ DataServiceLayer/models/Category.cs
@@ -3,8 +3,8 @@ namespace DataServiceLayer.Models;
 public class Category
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
 
     public ICollection<Product>? Products { get; set; }
 }
