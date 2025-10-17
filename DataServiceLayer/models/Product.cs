5690aa2aa2e2f513fddd71c20470a75c39943890
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-17 15:02:47 +0200

    refactor

--- DataServiceLayer/models/Product.cs
+++ DataServiceLayer/models/Product.cs
@@ -7,13 +7,13 @@ namespace DataServiceLayer.Models;
 public class Product
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public  string? Name { get; set; }
     public int SupplierId { get; set; }
     public int CategoryId { get; set; }
-    public required string QuantityPerUnit { get; set; }
+    public string? QuantityPerUnit { get; set; }
     public int UnitPrice { get; set; }
     public int UnitsInStock { get; set; }
 
     public Category? Category { get; set; }
-    public ICollection<OrderDetails> OrderDetails { get; set; }
+    public ICollection<OrderDetails>? OrderDetails { get; set; }
 }
